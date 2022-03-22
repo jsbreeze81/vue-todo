2ac062259a5edc9a6e19558ac5594c0fd08bb7a0
jsbreeze <js.breeze81@gmail.com>
2022-03-22 20:44:02 +0900

    day1

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <h2>Todo List</h2>
     
-    <TodoSimpleForm
+    <TodoSimpleFormVue
       @add-todo="addTodo"
     />
 
@@ -16,40 +16,38 @@
 
 <script>
 import {ref} from 'vue';
-import TodoSimpleForm from './components/TodoSimpleForm.vue';
+import TodoSimpleFormVue from './components/TodoSimpleForm.vue';
 import TodoList from './components/TodoList.vue';
 
 export default {
-    components: { 
-      TodoSimpleForm ,
-      TodoList
-    },
-    setup() {
-        const todo = ref("");
-        const todos = ref([]);
-        const addTodo = (todo) => {
-          todos.value.push(todo);
-        };
-        const deleteTodo = (index) => {
-            todos.value.splice(index, 1);
-        };
-        const toggleTodo = (index) => {
-            todos.value[index].completed = !todos.value[index].completed;
-        };
-        return {
-            todo,
-            todos,
-            addTodo,
-            deleteTodo,
-            toggleTodo
-        };
-    },
+  components:{
+    TodoSimpleFormVue,
+    TodoList
+  },
+  setup(){
+    const todos = ref([]);
+
+    const addTodo = (todo) => {
+      todos.value.push(todo)
+    }
+
+    const deleteTodo = (index) => {
+      todos.value.splice(index,1);
+    }
+
+    const toggleTodo = (index) => {
+      todos.value[index].completed = !todos.value[index].completed
+    }
+
+    return{
+      todos,
+      addTodo,
+      deleteTodo,
+      toggleTodo
+    }
+  }
 }
 </script>
 
 <style scoped>
-  .completed{
-    color:gray;
-    text-decoration: line-through;
-  }
 </style>
--- src/components/TodoSimpleForm.vue
+++ src/components/TodoSimpleForm.vue
@@ -1,20 +1,25 @@
 <template>
   <form
-    @submit.prevent="onSubmit"
+    @submit.prevent="addTodo"
   >
     <div class="d-flex">
       <div class="flex-grow-1 me-2">
         <input 
-          type="text"
+          type="text" 
           class="form-control"
           v-model="todo"
         >
       </div>
-      <button
-        type="submit"
-        class="btn btn-primary"
-      >ADD</button>
+      <div>
+        <button type="submit" class="btn btn-primary">ADD</button>
+      </div>
     </div>
+    <p 
+      class="mt-1 text-danger"
+      v-if="emptyInput"
+    >
+      내용을 입력해주세요
+    </p>
   </form>
 </template>
 
@@ -24,21 +29,27 @@ import {ref} from 'vue';
 export default {
   emits:['add-todo'],
   setup(props, {emit}){
+    const todo = ref('');
+    const emptyInput = ref(false);
 
-    const todo = ref("");
-    const onSubmit = () => {
-       emit('add-todo',{
-         id:Date.now(),
-         subject:todo.value,
-         completed:false
-       })
-      
-      todo.value = "";
-    };
-    
+    const addTodo = () => {
+      if(todo.value === ''){
+        emptyInput.value = true;
+      }else{
+        emit('add-todo',{
+          id : Date.now(),
+          subject : todo.value,
+          completed : false
+        })
+        todo.value = '';
+        emptyInput.value = false;
+      }
+    }
+
     return{
       todo,
-      onSubmit
+      emptyInput,
+      addTodo
     }
   }
 }
